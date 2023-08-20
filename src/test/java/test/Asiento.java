99b2eec9760dad145b74608a1e8270f6ecddd2b9
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-08-20 16:47:03 -0500

    Clase Asiento

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Asiento {
+	
+	String color;
+	
+	int precio;
+	
+	int registro;
+	
+	void CambiarColor (String color)
+	{
+		if (color == "rojo" || color == "verde" || color == "amarillo" || color == "negro" || color == "blanco")
+		{
+			this.color = color;
+		}
+	}
+	
+}
+

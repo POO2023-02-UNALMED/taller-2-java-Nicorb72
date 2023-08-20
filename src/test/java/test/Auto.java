0a1701922b8f1e952076aa30008f49f96b1e9823
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-08-20 17:44:04 -0500

    Clase auto

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,64 @@
+package test;
+
+public class Auto {
+	
+	String modelo;
+	
+	int precio;
+	
+	Asiento [] asientos;
+	
+	String marca;
+	
+	Motor motor;
+	
+	int registro;
+	
+	static int cantidadCreados;
+	
+	int cantidadAsientos()
+	{
+		int numAsientos = 0;
+		
+	    for (int i = 0 ; i < asientos.length; i++)
+	    	
+	    {
+	    	if (asientos [i] != null)
+	    	
+	    	{
+	    		numAsientos++;
+	    		
+	    	}
+	    	
+	    }
+	    
+	    return numAsientos;
+	}
+	
+	String verificarintegridad()
+	{
+		if ( registro == motor.registro)
+		{
+			for (int i = 0 ; i < asientos.length; i++)
+			{
+				if (asientos [i] != null)
+				
+				{
+					if (asientos[i].registro != registro)
+					{
+						return "Las piezas no son originales";
+						
+					}
+				}
+			}
+			return "Auto original";
+		}
+		else {
+			return "las piezas no son originales";
+		
+		}
+	}
+	
+	
+
+}
